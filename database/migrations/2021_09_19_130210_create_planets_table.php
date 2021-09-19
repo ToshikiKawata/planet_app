28d12895028690017016662576e84e94200ad11e
ToshikiKawata <jubei.animal@gmail.com>
2021-09-20 07:13:28 +0900

    惑星情報管理アプリ課題のPlanetモデル作成

---
+++ database/migrations/2021_09_19_130210_create_planets_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlanetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('planets', function (Blueprint $table) {
+            $table->string('name');
+            $table->string('english');
+            $table->integer('radius');
+            $table->integer('weight');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('planets');
+    }
+}
